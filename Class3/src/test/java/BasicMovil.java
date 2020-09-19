4a8a04721d2d4ea79f2fd9a56d61db42a8b87f3b
Jorge Mollericon <jlmollericon@gmail.com>
2020-09-19 09:42:18 -0400

    configure appium in BasicMovil.java

--- Class3/src/test/java/BasicMovil.java
+++ Class3/src/test/java/BasicMovil.java
@@ -1,12 +1,28 @@
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class BasicMovil {
+
+    /* Desired Capabilities
+    {
+        "deviceName": "Xperia E5",
+        "platformVersion": "6.0",
+        "appPackage": "com.android.calculator2",
+        "appActivity": ".Calculator",
+        "platformName": "Android"
+    } */
 
     @Before
     public void before() {
         System.out.println("before");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("deviceName", "Xperia E5");
+        capabilities.setCapability("platformVersion", "6.0");
+        capabilities.setCapability("appPackage", "com.android.calculator2");
+        capabilities.setCapability("appActivity", ".Calculator");
+        capabilities.setCapability("platformName", "Android");
     }
     @Test
     public void myFirstTest(){

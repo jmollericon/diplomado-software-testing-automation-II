a00a734ad73a698a3d243ce0d91450172856fcad
Jorge Mollericon <jlmollericon@gmail.com>
2020-09-19 10:23:50 -0400

    @Test add, verify add, ok.

--- Class3/src/test/java/BasicMovil.java
+++ Class3/src/test/java/BasicMovil.java
@@ -1,12 +1,16 @@
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 public class BasicMovil {
 
@@ -32,10 +36,24 @@ public class BasicMovil {
         capabilities.setCapability("platformName", "Android");
 
         this.driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+
+        // Implicit wait
+        this.driver.manage().timeouts().implicitlyWait(25, TimeUnit.SECONDS);
     }
     @Test
-    public void myFirstTest(){
-        System.out.println("my first test...");
+    public void add(){
+        // Press button 2
+        this.driver.findElement(By.id("com.android.calculator2:id/digit_2")).click();
+        // Press button +
+        this.driver.findElement(By.id("com.android.calculator2:id/op_add")).click();
+        // Press button 5
+        this.driver.findElement(By.id("com.android.calculator2:id/digit_5")).click();
+        // Press button =
+        this.driver.findElement(By.id("com.android.calculator2:id/result")).click();
+        // verification = 7
+        String actualResult = this.driver.findElement(By.id("result")).getText();
+        String expectedResult = "7";
+        Assert.assertEquals("Error, the sum is wrong.",expectedResult, actualResult);
     }
     @Test
     public void mySecondTest(){
@@ -44,5 +62,6 @@ public class BasicMovil {
     @After
     public void after() {
         System.out.println("after");
+        this.driver.quit();
     }
 }

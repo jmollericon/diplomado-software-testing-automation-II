5780bc8592eaab6c0b9c1f07f7c7271a4e7086e7
Jorge Mollericon <jlmollericon@gmail.com>
2020-10-03 15:52:09 -0400

    add configurations -> Configuration

---
+++ REST_API/src/test/java/configuration/Configuration.java
@@ -0,0 +1,9 @@
+package configuration;
+
+public class Configuration {
+
+    public static final String HOST="http://todo.ly/";
+    //  user:pwd ---> Basic base64
+    public static final String BASIC_AUTHENTICATION="Basic dWNiQHVjYi5jb206dWNiMTIz";
+    public static final String BASIC_AUTHENTICATION_HEADER="Authorization";
+}
